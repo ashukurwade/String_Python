ac12ff8a855843712238522c8c03c68b3dc0e87a
Ashutosh <ashutoshkurwade@gmail.com>
2022-11-26 19:33:48 +0530

    count the occurance of characters in string

--- occuranceString.py
+++ occuranceString.py
@@ -1,2 +1,15 @@
-m = " Pocket Book"
-print("Number of occurance of o:", m.count('o'))
+from itertools import count
+
+count = 0
+my_string = "Ashutosh"
+my_char ="h"
+
+for i in my_string:
+    if i == my_char:
+        count+= 1
+print(count)
+
+
+#another way
+#m = "Pocket Book"
+#print("Number of occurance of o:", m.count('o'))
