e64947c59b37c01e13e354c10d4fa15d4da0ade5
Ashutosh <ashutoshkurwade@gmail.com>
2022-12-01 22:33:11 +0530

    commonly used methods in python string

---
+++ stringMethods.py
@@ -0,0 +1,19 @@
+#lowercase
+m_string  = 'AsHuToSH'
+print(m_string.lower())
+
+
+#uppercase
+print("aSHutoSH".upper())
+
+#splitMethod
+print("This will split all words into a list".split())
+
+#joinMethod
+print(' '.join(['This', 'will', 'join', 'all', 'words', 'into', 'a', 'string']))
+
+#findMethod
+print('Happy New Year'.find('H'))
+
+#replaceMethod
+print('Happy New Year'.replace('Happy','Brilliant'))
