da95f9ad4ee99277ff67b46fc0687692407fae8d
Ashutosh <ashutoshkurwade@gmail.com>
2022-12-07 23:28:37 +0530

    remove characters from a string starting from zero up to n and return a new string

---
+++ removeFirstNCharactersString.py
@@ -0,0 +1,9 @@
+def remove_chars(word, n):
+    print("original String:",word)
+    x = word[n:]
+    return x
+
+print("Removing characters from a String")
+print(remove_chars("Ashutosh",7))
+print(remove_chars("Ashutosh",2))
+print(remove_chars("Ashutosh",3))
