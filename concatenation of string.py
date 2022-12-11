8c3020b6d89eec3b1ff81de7514ecb2ed9ae45a0
Ashutosh <ashutoshkurwade@gmail.com>
2022-12-11 23:34:14 +0530

    git push

--- concatenation of string.py
+++
@@ -1,9 +0,0 @@
-str1="We should not give up"
-str2="and"
-str3="we should not allow the problem to defeat us."
-
-# using +
-print(' str1 + str2 + str3 =', str1 +str2 + str3)
-
-#using *
-print("str1 * 3 * =",str1 * 3)
--- occurance of string .py
+++
@@ -1,2 +0,0 @@
-m = " Pocket Book"
-print("Number of occurance of o:", m.count('o'))
