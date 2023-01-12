c384f032b1fe49224ef71c43908306e58c453c25
Ashutosh Kurwade <ashutoshkurwade@gmail.com>
2023-01-12 23:44:55 +0530

    Creating String

---
+++ creatingString.py
@@ -0,0 +1,11 @@
+# Creating a String in Python
+# Strings in Python can be created using single quotes or double quotes or even triple quotes.
+
+string1=' Hello '
+print(string1)
+
+string2 = " World "
+print(string2)
+
+string3 =''' This world is but a canvas to our imagination '''
+print(string3)
