499715336d068dac47b9692e9393fdbdbbb61fc4
Ashutosh Kurwade <ashutoshkurwade@gmail.com>
2023-01-12 23:46:35 +0530

    ...

---
+++ string_1.py
@@ -0,0 +1,10 @@
+# A string is a data structure in Python that represents a sequence of characters.
+# It is an immutable data type, meaning that once you have created a string, you cannot change it.
+
+# Python does not have a character data type, a single character is simply a string with a length of 1.
+# Square brackets can be used to access elements of the string.
+
+
+
+
+print("The North Remembers")
