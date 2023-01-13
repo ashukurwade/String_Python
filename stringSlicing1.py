ba5556d2d005e419dfad08356ec3adcf1f685923
Ashutosh Kurwade <ashutoshkurwade@gmail.com>
2023-01-13 23:17:59 +0530

    String Slicing

---
+++ stringSlicing1.py
@@ -0,0 +1,10 @@
+# To access a range of characters in the String, the method of slicing is used.
+# Slicing in a String is done by using a Slicing operator (colon).
+
+string1 = "Virtualization"
+print(string1)
+
+print(string1[3])
+print(string1[1:7])
+print(string1[7:-2])
+print(string1[3:7])
